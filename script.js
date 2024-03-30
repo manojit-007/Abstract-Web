0b2217d92c4ec08d9ca0ec7dddc4f3d76ec0ffa8
manojit-007 <159749926+manojit-007@users.noreply.github.com>
2024-03-30 13:24:53 +0530

    1  more section complete

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 menuBtn = document.querySelector(".menu-btn")
 miniMenu = document.querySelector(".mini-menu")
 
+
 menuBtn.addEventListener("click", () => {
-    if (miniMenu.style.display === "none") {
-        miniMenu.style.display = "flex";
-    } else {
-        miniMenu.style.display = "none";
-    }
+    miniMenu.classList.toggle("active");
 });
+
+
+// menuBtn.addEventListener("click", () => {
+//     miniMenu.classList.add(".active")
+//     miniMenu.classList.remove(".active")
+// });
+
+// if (miniMenu.style.display === "none") {
+//     miniMenu.style.display = "flex";
+// } else {
+//     miniMenu.style.display = "none";
+// }
